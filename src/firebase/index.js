4f1a0ea5ae33df4516b60c0ec72838edc7e8a73d
Fiona Dong <fiona.dong3@student.tdsb.on.ca>
2024-12-10 13:54:05 -0500

    summative upd

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCVZurZ7jwvQi3HZqn-GLz7vyb0A6i-l9g",
+  authDomain: "summative-cc52f.firebaseapp.com",
+  projectId: "summative-cc52f",
+  storageBucket: "summative-cc52f.firebasestorage.app",
+  messagingSenderId: "496380123965",
+  appId: "1:496380123965:web:eaf68b8f712615ff798354"
+};
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
